68198a2251339f7df7b8153e86eee3a015cd6197
Griffin Brome <26681097+Griffin-Brome@users.noreply.github.com>
2025-09-06 16:45:10 -0700

    feat: add rust & python

--- scripts/toolbox.sh
+++ scripts/toolbox.sh
@@ -6,3 +6,9 @@
 # Update the container and install packages
 pacman -Syu
 grep -v '^#' ./toolbox.packages | xargs pacman -S --noconfirm
+
+# Install rust toolchain
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install uv for python tooling
+curl -LsSf https://astral.sh/uv/install.sh | sh
